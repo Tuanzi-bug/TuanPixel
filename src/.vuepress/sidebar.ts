e798770e98396f114a21c0a27c505640e879ee2f
TuanZi-bug <yangaoyu33@gmail.com>
2024-02-10 00:48:30 +0800

    :art:new years

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,25 +1,14 @@
-import { sidebar } from "vuepress-theme-hope";
+import { sidebar } from 'vuepress-theme-hope';
 
 export default sidebar({
-  "/": [
-    "",
+  '/': [
+    '',
+    'village/',
+    'road/',
     {
-      text: "案例",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
-    },
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
+      text: '幻灯片',
+      icon: 'person-chalkboard',
+      link: 'https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html',
     },
   ],
 });
